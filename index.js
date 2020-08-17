7a9c76559573e88f2bce8d9c6378128b8bd04343
dcbentley <danielcbentley@gmail.com>
2020-08-17 18:46:49 -0400

    complete index.js v1.0

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 // Require necessary NPM packages
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Instantiate express application object
+const app = express();
 
 // .use cors
+app.use(cors());
 
 // Add express.json
+app.use(express.json());
 
 // urlencoded
+app.use(express.urlencoded({ extended: true }));
+const imageController = require('./controllers/images');
+app.use('/api/images', imageController);
 
 // Require the user resources routes and controllers
+const imagesController = require('./controllers/images');
+app.use('/api/images', imagesController);
 
 // Handle Errors Server-wide
+// This is part of middleware that will be part of stretch goals
 
 // Define a port for the API to run on && define a PORT to use if port isn't found. (PORT || 4000)
+const port = process.env.PORT || 4000;
+
+// Run server on designated port
+app.listen(port, () => {
+	console.log('app is listening on port:', port);
+});
