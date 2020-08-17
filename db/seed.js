ac72d04ddd1e49c7617b74afb6ecfec9c277b659
dcbentley <danielcbentley@gmail.com>
2020-08-17 18:53:26 -0400

    initial scaffolding for seeds

---
+++ db/seed.js
@@ -0,0 +1,15 @@
+// The intent is to run this only once as it will both clear and seed the db with test info
+
+// Require the Image model
+
+// Require the data
+
+// Delete any existing documents in the Images collection
+
+// Use insertMany and feed it the seed data
+
+// Log a successful result
+
+// Log an error if things go sideways
+
+// Close the connection to the db after the process is complete 
