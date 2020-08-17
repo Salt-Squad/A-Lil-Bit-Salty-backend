ef19fb7518622d45621e6235d7e4cca32dea5ac2
samieSoa147 <sam789@live.com>
2020-08-17 15:02:46 -0400

    removed uneccesary model/controller

--- db/models/Name.js
+++
@@ -1,7 +0,0 @@
-// Require necessary NPM package(s) and point to db connection
-
-// Set var for nameSchema = new
-
-// set objects that will be part of the schema
-
-// Export module to mongoose ('Name', var for nameSchema)
