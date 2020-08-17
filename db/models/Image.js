0169405d2e07877a7dda90d3365f21315b17a61f
dcbentley <danielcbentley@gmail.com>
2020-08-17 14:43:14 -0400

    added scaffolding for Image.js

---
+++ db/models/Image.js
@@ -0,0 +1,7 @@
+// Require necessary NPM package(s) and point to db connection
+
+// Set var for imageSchema = new
+
+// set objects that will be part of the schema
+
+// Export module to mongoose ('Image', var for imageSchema)
