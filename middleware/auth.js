8036fdf642fd1248c34445ce7df36bafdb47646c
dcbentley <danielcbentley@gmail.com>
2020-08-17 15:03:02 -0400

    started auth.js scaffolding

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+// Require necessary NPM packages 
+
+
+// Create a secret to be used to encrypt/decrypt the toke
+// String can be anything
+
+// Require specific 'strategy' we'll use to perform authentication
+// Require the method that will handle extracting the token from each of the request sent by the clients
+
+// Minimum require options for passport-jwt
