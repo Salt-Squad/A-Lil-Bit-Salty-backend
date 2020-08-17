9a1fc4f180e39a12a84f5b9087b79c786f22db1b
dcbentley <danielcbentley@gmail.com>
2020-08-17 14:17:20 -0400

    added index scaffolding

---
+++ index.js
@@ -0,0 +1,15 @@
+// Require necessary NPM packages
+
+// Instantiate express application object
+
+// .use cors
+
+// Add express.json
+
+// urlencoded
+
+// Require the user resources routes and controllers
+
+// Handle Errors Server-wide
+
+// Define a port for the API to run on && define a PORT to use if port isn't found. (PORT || 4000)
